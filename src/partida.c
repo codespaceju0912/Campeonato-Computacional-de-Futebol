940a775bb115e3ad149e07346a371e778754adae
codespaceju0912 <juliaalmeidanasc8@gmail.com>
2025-11-05 17:08:53 -0300

    iniciei o .c e o .h de partidas

---
+++ src/partida.c
@@ -0,0 +1,11 @@
+#include "partida.h"
+#include <stdio.h>
+
+typedef struct {
+    int id;
+    int time1;
+    int time2;
+    int golsTime1;
+    int golsTime2;
+} Partida;
+
---
+++ src/partida.h
@@ -0,0 +1,11 @@
+#ifndef PARTIDA_H
+#define PARTIDA_H
+
+typedef enum{
+    TIME1, //Indica o nó que contém dados do time 1
+    TIME2 // Indica o nó que contém dados do time 2
+}NodeTime;
+
+
+
+#endif 
