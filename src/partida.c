a5e05d9d2f9f1839f9a7945858e6cdcbc5ae7b7b
codespaceju0912 <juliaalmeidanasc8@gmail.com>
2025-11-07 18:21:56 -0300

    adicionei os documentos importantes e testei leitura de arquivo

--- src/partida.c
+++ src/partida.c
@@ -1,11 +1,17 @@
 #include "partida.h"
 #include <stdio.h>
+#define MAX_PARTIDA 90
 
 typedef struct partida {
     int id;
-    int time1;
-    int time2;
+    int* time1;
+    int* time2;
     int golsTime1;
     int golsTime2;
 } Partida;
 
+typedef struct bdpartida{
+    int partida[MAX_PARTIDA];
+}BDPartida;
+
+FILE 
