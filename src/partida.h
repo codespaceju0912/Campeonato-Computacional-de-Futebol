338460eefb284b70bdb56c550f50ce1b56ba4084
ElisaAndradedeJesus <elisaaj.es@gmail.com>
2025-11-09 09:05:37 -0300

    arquivos.h

--- src/partida.h
+++ src/partida.h
@@ -1,5 +1,17 @@
 #ifndef PARTIDA_H
 #define PARTIDA_H
 
+typedef struct {
+    int id; //identificador
 
-#endif 
+    int time1; //id do time 1
+    int time2; //id do time 2
+
+    int golsTime1;
+    int golsTime2;
+
+} Partida;
+
+void Partida_imprimir_resultado(const Partida *p, const char *nome1, const char *nome2);
+
+#endif
